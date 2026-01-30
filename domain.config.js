3bdb3de5e7b6898ba47018b7dcfa3b5d185ba21d
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2026-01-30 14:03:19 +0700

    Update domain.config.js

--- domain.config.js
+++ domain.config.js
@@ -1,7 +1,7 @@
 // Centralized domain configuration
 // This file uses CommonJS for compatibility with next.config.js
 module.exports = {
-    domain: "https://hotnews.livextop.com"
+    domain: "https://hotnews.daily24.blog"
 };
 
 // To change domain, update the URL above
